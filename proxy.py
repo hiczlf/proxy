4ac0eca0d8eac41e0d54fcae58d6b8c3d7bea91f
lifenglifeng001 <lifenglifeng001@gmail.com>
2014-06-23 09:18:56 +0800

    kll

--- proxy.py
+++ proxy.py
@@ -5,7 +5,7 @@ import argparse
 from twisted.internet import reactor
 from twisted.web import proxy, http
 
-IP_LIST = ['116.24.29.200', '113.89.9.136', '183.14.11.19']
+IP_LIST = ['116.24.29.200', '113.89.9.136', '183.14.11.19', '113.108.68.161']
 
 
 def parse_args():
