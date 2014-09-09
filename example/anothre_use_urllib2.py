b708b66ca2f66ccec7ec86554ce7a5af69ee95de
lifenglifeng001 <lifenglifeng001@gmail.com>
2014-09-09 11:49:52 +0800

    add some example

---
+++ example/anothre_use_urllib2.py
@@ -0,0 +1,13 @@
+import urllib2
+
+proxy = {'http': 'http://lf:lf@192.168.0.49:9999',
+         'https': 'http://lf:lf@192.168.0.49:9999'}
+proxy_support = urllib2.ProxyHandler(proxy)
+opener = urllib2.build_opener(proxy_support)
+urllib2.install_opener(opener)
+# url = "http://baidu.com"
+url = "https://github.com"
+req = urllib2.Request(url)
+page = urllib2.urlopen(req, timeout=15)
+hc = page.read()
+print(hc)
---
+++ example/use_urllib2.py
@@ -0,0 +1,13 @@
+import urllib2
+
+proxy = urllib2.ProxyHandler({
+    'http': 'http://lf:lf@192.168.0.49:9999',
+    'https': 'http://lf:lf@192.168.0.49:9999',
+})
+auth = urllib2.HTTPBasicAuthHandler()
+opener = urllib2.build_opener(proxy, auth, urllib2.HTTPHandler)
+
+# conn = urllib2.urlopen('http://dc.icgoo.net')
+conn = urllib2.urlopen('https://github.com')
+return_str = conn.read()
+print(return_str)
